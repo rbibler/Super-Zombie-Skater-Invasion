95a69820cc250dc76235c515c0fc2be0131184a8
rbibler <rbibler01@hotmail.com>
2016-01-28 08:34:27 -0500

    Fixed continue screen
    And the bail anims.

--- Assets/Scripts/ContinueScreenScript.cs
+++ Assets/Scripts/ContinueScreenScript.cs
@@ -5,13 +5,36 @@ public class ContinueScreenScript : MonoBehaviour {
 
 	public LevelManager manager;
 	public AudioClip clip;
+	public Vector2 continueCursorPos;
+	public Vector2 passwordCursorPos;
+	public GameObject cursor;
+	private bool password;
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (!password) {
+				cursor.transform.position = passwordCursorPos;
+				password = true;
+			} else {
+				cursor.transform.position = continueCursorPos;
+				password = false;
+			}
+		} else if (Input.GetKeyDown (KeyCode.Return)) {
+			if(!password) {
+				AudioSource.PlayClipAtPoint(clip, Vector3.left);
+				Invoke("Continue", 2.0f);
+			}
+		}
+	}
 	
 	void OnMouseDown() {
-		AudioSource.PlayClipAtPoint(clip, Vector3.left);
+		AudioSource.PlayClipAtPoint(clip, continueCursorPos);
 		Invoke("Continue", 2.0f);
 	}
 	
 	void Continue() {
 		manager.ReloadPreviousLevel();
 	}
+
+
 }
